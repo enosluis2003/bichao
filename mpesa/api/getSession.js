6f317e5d74d170faf02b0e6543a34d344f610a5d
Peixe D3v <75576088+enosluis2003@users.noreply.github.com>
2026-05-17 22:06:00 +0200

    Update getSession.js

--- mpesa/api/getSession.js
+++ mpesa/api/getSession.js
@@ -3,8 +3,11 @@ import crypto from 'crypto';
 export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).json({ error: 'Método não permitido' });
 
+    // A chave pública gigante
     const publicKey = "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAszE+xAKVB9HRarr6/uHYYAX/RdD6KGVIGlHv98QKDIH26ldYJQ7zOuo9qEscO0M1psSPe/67AWYLEXh13fbtcSKGP6WFjT9OY6uV5ykw9508x1sW8UQ4ZhTRNrlNsKizE/glkBfcF2lwDXJGQennwgickWz7VN+AP/1c4DnMDfcl8iVIDlsbudFoXQh5aLCYl+XOMt/vls5a479PLMkPcZPOgMTCYTCE6ReX3KD2aGQ62uiu2T4mK+7Z6yvKvhPRF2fTKI+zOFWly//IYlyB+sde42cIU/588msUmgr3G9FYyN2vKPVy/MhIZpiFyVc3vuAAJ/mzue5p/G329wzgcz0ztyluMNAGUL9A4ZiFcKOebT6y6IgIMBeEkTwyhsxRHMFXlQRgTAufaO5hiR/usBMkoazJ6XrGJB8UadjH2m2+kdJIieI4FbjzCiDWKmuM58rllNWdBZK0XVHNsxmBy7yhYw3aAIhFS0fNEuSmKTfFpJFMBzIQYbdTgI28rZPAxVEDdRaypUqBMCq4OstCxgGvR3Dy1eJDjlkuiWK9Y9RGKF8HOI5a4ruHyLheddZxsUihziPF9jKTknsTZtF99eKTIjhV7qfTzxXq+8GGoCEABIyu26LZuL8X12bFqtwLAcjfjoB7HlRHtPszv6PJ0482ofWmeH0BE8om7VrSGxsCAwEAAQ==";
-    const apiKey = "6bc4157dbee34d409118e0978dc6dd17";
+    
+    // A sua API Key EXCLUSIVA que você gerou agora
+    const apiKey = "9TdohQd9ortOeqcJquB5XaFxRWdjaaOX";
 
     try {
         const pemKey = `-----BEGIN PUBLIC KEY-----\n${publicKey.match(/.{1,64}/g).join('\n')}\n-----END PUBLIC KEY-----`;
@@ -14,8 +17,8 @@ export default async function handler(req, res) {
             Buffer.from(apiKey)
         ).toString('base64');
 
-        // URL ATUALIZADA PARA MOÇAMBIQUE (vodacomMZ)
-        const response = await fetch('https://openapi.m-pesa.com/sandbox/ipg/v2/vodacomMZ/getSession/', {
+        // URL corrigida para vodacomMOZ
+        const response = await fetch('https://openapi.m-pesa.com/sandbox/ipg/v2/vodacomMOZ/getSession/', {
             method: 'GET',
             headers: { 'Authorization': `Bearer ${encryptedKey}`, 'Origin': '*' }
         });
